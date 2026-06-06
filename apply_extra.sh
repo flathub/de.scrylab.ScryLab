4643629318ae7214d63ba3f597ff896d5223db06
nik661 <nik.anonym@posteo.de>
2026-06-06 15:58:07 +0200

    Use archive instead of extra_data

--- apply_extra.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-set -eu
-tar -xzf scrylab.tar.gz
-rm -f scrylab.tar.gz
--- scrylab-wrapper.sh
+++ scrylab-wrapper.sh
@@ -2,12 +2,12 @@
 
 set -eu
 
-APP_DIR="/app/extra/ScryLab.dist"
+APP_DIR="/app/lib/ScryLab.dist"
 APP_BIN="$APP_DIR/ScryLab"
 
 if [ ! -x "$APP_BIN" ]; then
     echo "ScryLab: bundled binary not found at $APP_BIN" >&2
-    echo "The extra-data step (apply_extra) likely did not run. Re-install the Flatpak." >&2
+    echo "The bundled app files are missing. Re-install the Flatpak." >&2
     exit 1
 fi
 
